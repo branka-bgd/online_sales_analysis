48415dcc046508c6f11de8f167611ac9750ae28d
Branka <brnk.bgd@gmail.com>
2026-03-11 19:42:21 +0100

    Added Cart script and modified main

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 prm1 = ProductManager([])
 
 
-prm1.add_product("Laptop", 1200, 10)
-prm1.add_product("Mouse", 50, 50)  
-prm1.add_product("Monitor", 450, 5)
-prm1.display_products()
-prm1.total_value()
+#prm1.add_product("Laptop", 1200, 10)
+#prm1.add_product("Mouse", 50, 50)  
+#prm1.add_product("Monitor", 450, 5)
+#prm1.display_products()
+#prm1.total_value()
 
+products = [
+    Product("Laptop", 1200, 2),
+    Product("Mouse", 20, 5),
+    Product("Monitor", 250, 3)
+]
 
+my_cart = Cart()
+for product in products:
+    my_cart.add_item(product, 1)
+my_cart.view_cart()
+my_cart.total_amount()
